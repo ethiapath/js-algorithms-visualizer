6bfbec7c5b8a99e16c53116c0a2d261f2ef83647
J <jeremiaxhlin@gmail.com>
2019-06-25 02:04:29 +0000

    ADD: comments to Stack class

--- src/lib/data-structures/stack/Stack.js
+++ src/lib/data-structures/stack/Stack.js
@@ -3,24 +3,42 @@ class Stack {
     this.items = items;
   }
 
+  /**
+   * Is the stack empty?
+   */
   isEmpty() {
     return this.items.length === 0;
   }
+
+  /**
+   * Preview the top item in the stack w/o modifying the stack
+   */
   peek() {
     if (this.isEmpty()) return null;
     const [lastItem] = this.items.slice(-1);
     return lastItem;
   }
 
+  /**
+   * Push an item to the top of the stack
+   *
+   * @param {*} item
+   */
   push(item) {
     this.items = [...this.items, item];
     return this.items;
   }
 
+  /**
+   * Remove and return the top item from the stack
+   */
   pop() {
     return this.items.pop() || null;
   }
 
+  /**
+   * Get a string representation of the stack from bottom => top
+   */
   toString() {
     return this.items.toString();
   }
